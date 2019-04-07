71334593a4a667d4b94e25a9af90c45fb5746e3b
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-04-07 22:03:21 +0430

    do not compile kernel every time

--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -4,35 +4,43 @@ use self::ocl::ProQue;
 use self::ocl::prm::Ulong4;
 use pairing::bls12_381::{Bls12, Fr};
 
+static KERNEL_SRC : &str = include_str!("fft.cl");
+static mut PRO_QUE : Option<ProQue> = None;
+
 pub fn fft(a: &mut [Fr], omega: &Fr, lgn: u32) -> ocl::Result<()> {
-    let ta = unsafe { std::mem::transmute::<&mut [Fr], &mut [Ulong4]>(a) };
-    let tomega = *(unsafe { std::mem::transmute::<&Fr, &Ulong4>(omega) });
+    // WARNING: This is a big mess!
+    unsafe {
+        let ta = unsafe { std::mem::transmute::<&mut [Fr], &mut [Ulong4]>(a) };
+        let tomega = *(unsafe { std::mem::transmute::<&Fr, &Ulong4>(omega) });
 
-    // This is getting recompiled again and again. Make it a global variable!
-    let src = include_str!("fft.cl");
-    let pro_que = ProQue::builder()
-        .src(src)
-        .dims(ta.len())
-        .build()?;
+        if let Some(ref mut value) = PRO_QUE { }
+        else { PRO_QUE = Some(ProQue::builder().src(KERNEL_SRC).dims(a.len()).build()?); }
+        let pro_que : &mut ProQue =
+            match PRO_QUE {
+                Some(ref mut x) => &mut *x,
+                None => panic!(),
+            };
 
-    let buffer = pro_que.create_buffer::<Ulong4>()?;
+        // WARNING: It is creating a new buffer every time!
+        let buffer = pro_que.create_buffer::<Ulong4>()?;
 
-    let mut vec = vec![Ulong4::zero(); buffer.len()];
-    for i in 0..buffer.len() { vec[i] = ta[i]; }
+        let mut vec = vec![Ulong4::zero(); buffer.len()];
+        for i in 0..buffer.len() { vec[i] = ta[i]; }
 
-    buffer.write(&vec).enq()?;
+        buffer.write(&vec).enq()?;
 
-    let kernel = pro_que.kernel_builder("fft")
-        .arg(&buffer)
-        .arg(ta.len() as u32)
-        .arg(lgn as u32)
-        .arg(tomega)
-        .build()?;
+        let kernel = pro_que.kernel_builder("fft")
+            .arg(&buffer)
+            .arg(ta.len() as u32)
+            .arg(lgn as u32)
+            .arg(tomega)
+            .build()?;
 
-    unsafe { kernel.enq()?; }
+        unsafe { kernel.enq()?; }
 
-    buffer.read(&mut vec).enq()?;
+        buffer.read(&mut vec).enq()?;
 
-    for i in 0..a.len() { ta[i] = vec[i]; }
+        for i in 0..a.len() { ta[i] = vec[i]; }
+    }
     Ok(())
 }
