9368e2b7279e27fdea0915a8b0e448e8b1ca7438
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-04-14 20:46:57 +0430

    make radix2 fft inplace

--- src/gpu/fft.cl
+++ src/gpu/fft.cl
@@ -1,33 +1,25 @@
-__kernel void radix2_fft(__global ulong4* src,
-                  __global ulong4* dst,
+__kernel void radix2_fft(__global ulong4* buffer,
                   uint n,
                   uint lgn,
                   ulong4 om,
                   uint lgm) {
 
-  __global uint256 *x = src;
-  __global uint256 *y = dst;
+  int index = get_global_id(0);
+
+  __global uint256 *elems = buffer;
   uint256 omega = *(uint256*)&om;
 
-  uint i = get_global_id(0);
-  uint t = n >> 1;
+  uint works = n >> (lgm + 1);
   uint m = 1 << lgm;
 
-  uint k = i & (m - 1);
+  uint32 k = index >> lgm << (lgm + 1);
+  uint32 j = index & (m - 1);
+  uint256 w = powmod(omega, (n >> lgm >> 1) * j);
 
-  uint256 u0;
-  u0 = x[i];
-  uint256 u1;
-  u1 = x[i+t];
-
-  uint256 twiddle = powmod(omega, (n >> lgm >> 1) * k);
-  u1 = mulmod(u1, twiddle);
-
-  uint256 tmp = submod(u0, u1);
-  u0 = addmod(u0, u1);
-  u1 = tmp;
-
-  uint j = (i<<1) - k;
-  y[j] = u0;
-  y[j+m] = u1;
+  uint256 t = elems[k+j+m];
+  t = mulmod(t, w);
+  uint256 tmp = elems[k+j];
+  tmp = submod(tmp, t);
+  elems[k+j+m] = tmp;
+  elems[k+j] = addmod(elems[k+j], t);
 }
--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -9,39 +9,49 @@ static KERNEL_SRC : &str = include_str!("fft.cl");
 
 pub struct Kernel {
     proque: ProQue,
-    fft_src_buffer: Buffer<Ulong4>,
-    fft_dst_buffer: Buffer<Ulong4>
+    fft_buffer: Buffer<Ulong4>
 }
 
 pub fn create_kernel(n: u32) -> Kernel {
     let src = format!("{}\n{}", UINT256_SRC, KERNEL_SRC);
     let pq = ProQue::builder().src(src).dims(n).build().expect("Cannot create kernel!");
     let src = pq.create_buffer::<Ulong4>().expect("Cannot allocate buffer!");
-    let dst = pq.create_buffer::<Ulong4>().expect("Cannot allocate buffer!");
-    Kernel { proque: pq, fft_src_buffer: src, fft_dst_buffer: dst }
+    Kernel { proque: pq, fft_buffer: src }
 }
 
 impl Kernel {
 
     pub fn radix2_fft(&mut self, a: &mut [Fr], omega: &Fr, lgn: u32) -> ocl::Result<()> {
+        fn bitreverse(mut n: u32, l: u32) -> u32 {
+            let mut r = 0;
+            for _ in 0..l {
+                r = (r << 1) | (n & 1);
+                n >>= 1;
+            }
+            r
+        }
 
         let ta = unsafe { std::mem::transmute::<&mut [Fr], &mut [Ulong4]>(a) };
+        for k in 0..(a.len() as u32) {
+            let rk = bitreverse(k, lgn);
+            if k < rk {
+                ta.swap(rk as usize, k as usize);
+            }
+        }
+
         let tomega = *(unsafe { std::mem::transmute::<&Fr, &Ulong4>(omega) });
 
-        let mut vec = vec![Ulong4::zero(); self.fft_src_buffer.len()];
-        for i in 0..self.fft_src_buffer.len() { vec[i] = ta[i]; }
+        let mut vec = vec![Ulong4::zero(); self.fft_buffer.len()];
+        for i in 0..self.fft_buffer.len() { vec[i] = ta[i]; }
 
-        self.fft_src_buffer.write(&vec).enq()?;
-
-        let mut in_src = true;
+        self.fft_buffer.write(&vec).enq()?;
         let n = 1 << lgn;
 
         for lgm in 0..lgn {
 
             let kernel = self.proque.kernel_builder("radix2_fft")
                 .global_work_size([n >> 1])
-                .arg(if in_src { &self.fft_src_buffer } else { &self.fft_dst_buffer })
-                .arg(if in_src { &self.fft_dst_buffer } else { &self.fft_src_buffer })
+                .arg(&self.fft_buffer)
                 .arg(ta.len() as u32)
                 .arg(lgn as u32)
                 .arg(tomega)
@@ -49,15 +59,9 @@ impl Kernel {
                 .build()?;
 
             unsafe { kernel.enq()?; }
-
-            in_src = !in_src;
         }
 
-        if in_src {
-            self.fft_src_buffer.read(&mut vec).enq()?;
-        } else {
-            self.fft_dst_buffer.read(&mut vec).enq()?;
-        }
+        self.fft_buffer.read(&mut vec).enq()?;
 
         for i in 0..a.len() { ta[i] = vec[i]; }
         Ok(())
