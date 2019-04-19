d23ba9785c8fbb8e7acdfd1381d786ab059e5562
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-04-19 18:10:47 +0430

    remove old kernels

--- src/gpu/fft.cl
+++ src/gpu/fft.cl
@@ -1,29 +1,3 @@
-__kernel void custom_radix2_fft(__global ulong4* buffer,
-                  uint n,
-                  uint lgn,
-                  ulong4 om,
-                  uint lgm) {
-
-  int index = get_global_id(0);
-
-  __global uint256 *elems = buffer;
-  uint256 omega = *(uint256*)&om;
-
-  uint works = n >> (lgm + 1);
-  uint m = 1 << lgm;
-
-  uint32 k = index >> lgm << (lgm + 1);
-  uint32 j = index & (m - 1);
-  uint256 w = powmod(omega, (n >> lgm >> 1) * j);
-
-  uint256 t = elems[k+j+m];
-  t = mulmod(t, w);
-  uint256 tmp = elems[k+j];
-  tmp = submod(tmp, t);
-  elems[k+j+m] = tmp;
-  elems[k+j] = addmod(elems[k+j], t);
-}
-
 __kernel void bealto_radix2_fft(__global ulong4* src,
                   __global ulong4* dst,
                   uint n,
--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -41,57 +41,11 @@ pub fn find_gpu() -> bool {
         //     println!("Device Name: {:?}, Vendor: {:?}", device.name().unwrap(),
         //         device.vendor().unwrap());
         // }
-    }    
+    }
     test
 }
 
 impl FFT_Kernel {
-
-    pub fn custom_radix2_fft(&mut self, a: &mut [Fr], omega: &Fr, lgn: u32) -> ocl::Result<()> {
-        fn bitreverse(mut n: u32, l: u32) -> u32 {
-            let mut r = 0;
-            for _ in 0..l {
-                r = (r << 1) | (n & 1);
-                n >>= 1;
-            }
-            r
-        }
-
-        let ta = unsafe { std::mem::transmute::<&mut [Fr], &mut [Ulong4]>(a) };
-        for k in 0..(a.len() as u32) {
-            let rk = bitreverse(k, lgn);
-            if k < rk {
-                ta.swap(rk as usize, k as usize);
-            }
-        }
-
-        let tomega = *(unsafe { std::mem::transmute::<&Fr, &Ulong4>(omega) });
-
-        let mut vec = vec![Ulong4::zero(); self.fft_buffer.len()];
-        for i in 0..self.fft_buffer.len() { vec[i] = ta[i]; }
-
-        self.fft_buffer.write(&vec).enq()?;
-        let n = 1 << lgn;
-
-        for lgm in 0..lgn {
-
-            let kernel = self.proque.kernel_builder("custom_radix2_fft")
-                .global_work_size([n >> 1])
-                .arg(&self.fft_buffer)
-                .arg(ta.len() as u32)
-                .arg(lgn as u32)
-                .arg(tomega)
-                .arg(lgm)
-                .build()?;
-
-            unsafe { kernel.enq()?; }
-        }
-
-        self.fft_buffer.read(&mut vec).enq()?;
-
-        for i in 0..a.len() { ta[i] = vec[i]; }
-        Ok(())
-    }
 
     pub fn bealto_radix2_fft(&mut self, a: &mut [Fr], omega: &Fr, lgn: u32) -> ocl::Result<()> {
 
