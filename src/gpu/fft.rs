98b6883a7f365083e1cd16f70abbb177f18d2d44
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-05-10 05:28:53 +0430

    optimal for gtx750ti, should still implement mapping

--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -1,21 +1,18 @@
-use ocl::{ProQue, Buffer, MemFlags, MapFlags, MemMap};
+use ocl::{ProQue, Buffer, MemFlags, MemMap};
 use ocl::prm::Ulong4;
 use pairing::bls12_381::Fr;
 use std::cmp;
 use ff::Field;
-use futures::{stream, Future, Sink, Stream, Join};
-use futures::sync::mpsc::{self, Sender};
-use futures_cpupool::{CpuPool, CpuFuture};
 
 static UINT256_SRC : &str = include_str!("uint256.cl");
 static KERNEL_SRC : &str = include_str!("fft.cl");
-const MAX_RADIX_DEGREE : u32 = 1; // Radix2
-const MAX_LOCAL_WORK_SIZE_DEGREE : u32 = 0; // 1
+const MAX_RADIX_DEGREE : u32 = 8; // Radix2
+const MAX_LOCAL_WORK_SIZE_DEGREE : u32 = 7; // 1
 
 pub struct FFTKernel {
     proque: ProQue,
-    fft_src_buffer: Buffer<Ulong4>, fft_src_map: MemMap<Ulong4>,
-    fft_dst_buffer: Buffer<Ulong4>, fft_dst_map: MemMap<Ulong4>,
+    fft_src_buffer: Buffer<Ulong4>,
+    fft_dst_buffer: Buffer<Ulong4>,
     fft_pq_buffer: Buffer<Ulong4>
 }
 
@@ -25,17 +22,15 @@ pub fn initialize(n: u32) -> FFTKernel {
     let srcbuff = Buffer::builder().queue(pq.queue().clone())
         .flags(MemFlags::new().read_write()).len(n)
         .build().expect("Cannot allocate buffer!");
-    let srcmap = unsafe { srcbuff.map().enq().expect("Cannot map buffer!") };
     let dstbuff = Buffer::builder().queue(pq.queue().clone())
         .flags(MemFlags::new().read_write()).len(n)
         .build().expect("Cannot allocate buffer!");
-    let dstmap = unsafe { dstbuff.map().enq().expect("Cannot map buffer!") };
     let pqbuff = Buffer::builder().queue(pq.queue().clone())
         .flags(MemFlags::new().read_write()).len(1 << MAX_RADIX_DEGREE >> 1)
         .build().expect("Cannot allocate buffer!");
     FFTKernel {proque: pq,
-                fft_src_buffer: srcbuff, fft_src_map: srcmap,
-                fft_dst_buffer: dstbuff, fft_dst_map: dstmap,
+                fft_src_buffer: srcbuff,
+                fft_dst_buffer: dstbuff,
                 fft_pq_buffer: pqbuff}
 }
 
@@ -43,32 +38,20 @@ impl FFTKernel {
 
     fn radix_fft_round(&mut self, a: &mut [Ulong4], omega: &Ulong4, lgn: u32, lgp: u32, deg: u32, in_src: bool) -> ocl::Result<()> {
         let n = 1 << lgn;
-        if deg != 1 {
-            let lwsd = cmp::min(deg - 1, MAX_LOCAL_WORK_SIZE_DEGREE);
-            let kernel = self.proque.kernel_builder("radix_fft")
-                .global_work_size([n >> deg << lwsd])
-                .local_work_size(1 << lwsd)
-                .arg(if in_src { &self.fft_src_buffer } else { &self.fft_dst_buffer })
-                .arg(if in_src { &self.fft_dst_buffer } else { &self.fft_src_buffer })
-                .arg(&self.fft_pq_buffer)
-                .arg_local::<Ulong4>(1 << deg)
-                .arg(a.len() as u32)
-                .arg(omega)
-                .arg(lgp)
-                .arg(deg)
-                .build()?;
-            unsafe { kernel.enq()?; }
-        } else {
-            let kernel = self.proque.kernel_builder("radix2_fft")
-                .global_work_size([n >> 1])
-                .arg(if in_src { &self.fft_src_buffer } else { &self.fft_dst_buffer })
-                .arg(if in_src { &self.fft_dst_buffer } else { &self.fft_src_buffer })
-                .arg(a.len() as u32)
-                .arg(omega)
-                .arg(lgp)
-                .build()?;
-            unsafe { kernel.enq()?; }
-        }
+        let lwsd = cmp::min(deg - 1, MAX_LOCAL_WORK_SIZE_DEGREE);
+        let kernel = self.proque.kernel_builder("radix_fft")
+            .global_work_size([n >> deg << lwsd])
+            .local_work_size(1 << lwsd)
+            .arg(if in_src { &self.fft_src_buffer } else { &self.fft_dst_buffer })
+            .arg(if in_src { &self.fft_dst_buffer } else { &self.fft_src_buffer })
+            .arg(&self.fft_pq_buffer)
+            .arg_local::<Ulong4>(1 << deg)
+            .arg(a.len() as u32)
+            .arg(omega)
+            .arg(lgp)
+            .arg(deg)
+            .build()?;
+        unsafe { kernel.enq()?; }
         Ok(())
     }
 
@@ -88,7 +71,6 @@ impl FFTKernel {
     }
 
     pub fn radix_fft(&mut self, a: &mut [Fr], omega: &Fr, lgn: u32) -> ocl::Result<()> {
-        let thread_pool = CpuPool::new_num_cpus();
         let n = 1 << lgn;
 
         let ta = unsafe { std::mem::transmute::<&mut [Fr], &mut [Ulong4]>(a) };
@@ -96,7 +78,7 @@ impl FFTKernel {
 
         self.setup_pq(omega, n);
 
-        self.fft_src_map.copy_from_slice(ta); // memcpy
+        self.fft_src_buffer.write(&*ta).enq()?;
         let mut in_src = true;
         let mut lgp = 0u32;
         while lgp < lgn {
@@ -107,45 +89,9 @@ impl FFTKernel {
             lgp += deg;
             in_src = !in_src;
         }
+        if in_src { self.fft_src_buffer.read(ta).enq()?; }
+        else { self.fft_dst_buffer.read(ta).enq()?; }
         self.proque.finish(); // Wait for kernel
-
-        if in_src { 
-          //ta.copy_from_slice(&self.fft_src_map);
-          // self.fft_src_buffer.read(&mut vec).enq()?;
-          let future_read_data = unsafe {
-              self.fft_src_buffer.cmd().map()
-                  .flags(MapFlags::new().read())
-                  //.ewait(&kern_event)
-                  .enq_async()?
-          };
-          let read = future_read_data.and_then(move |mut data| {
-            let mut i = 0;
-            for val in data.iter_mut() {
-              ta[i] = *val;
-              i = i +1; 
-            }
-            Ok(())
-          });
-          let spawned_read = thread_pool.spawn(read).wait().unwrap();
-        }
-        else { 
-          //ta.copy_from_slice(&self.fft_dst_map);
-          let future_read_data = unsafe {
-              self.fft_dst_buffer.cmd().map()
-                  .flags(MapFlags::new().read())
-                  //.ewait(&kern_event)
-                  .enq_async()?
-          };
-          let read = future_read_data.and_then(move |mut data| {
-            let mut i = 0;
-            for val in data.iter_mut() {
-              ta[i] = *val;
-              i = i +1; 
-            }
-            Ok(())
-          });
-          let spawned_read = thread_pool.spawn(read).wait().unwrap();
-        }
 
         Ok(())
     }
