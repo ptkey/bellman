100a1e0ed8436bbb85c3fa5ac0969b82e5dff8ed
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-06-06 15:30:25 +0430

    support g2

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -1,6 +1,6 @@
 use ocl::{ProQue, Buffer, MemFlags};
 use ocl::traits::OclPrm;
-use paired::bls12_381::{FrRepr, G1Affine, G1};
+use paired::bls12_381::{FrRepr, G1Affine, G1, G2Affine, G2};
 use std::cmp;
 use std::sync::Arc;
 use ocl::prm::{Ulong4, Uchar};
@@ -21,10 +21,24 @@ unsafe impl OclPrm for G1AffineStruct { }
 pub struct G1ProjectiveStruct { x: FqStruct, y: FqStruct, z: FqStruct }
 unsafe impl OclPrm for G1ProjectiveStruct { }
 
+#[derive(PartialEq, Debug, Clone, Copy, Default)]
+pub struct Fq2Struct { c0: FqStruct, c1: FqStruct }
+unsafe impl OclPrm for Fq2Struct { }
+
+#[derive(PartialEq, Debug, Clone, Copy, Default)]
+pub struct G2AffineStruct { x: Fq2Struct, y: Fq2Struct, inf: bool }
+unsafe impl OclPrm for G2AffineStruct { }
+
+#[derive(PartialEq, Debug, Clone, Copy, Default)]
+pub struct G2ProjectiveStruct { x: Fq2Struct, y: Fq2Struct, z: Fq2Struct }
+unsafe impl OclPrm for G2ProjectiveStruct { }
+
 pub struct MultiexpKernel {
     proque: ProQue,
     g1_base_buffer: Buffer<G1AffineStruct>,
     g1_result_buffer: Buffer<G1ProjectiveStruct>,
+    g2_base_buffer: Buffer<G2AffineStruct>,
+    g2_result_buffer: Buffer<G2ProjectiveStruct>,
     exp_buffer: Buffer<Ulong4>,
     dm_buffer: Buffer<Uchar>
 }
@@ -40,13 +54,21 @@ impl MultiexpKernel {
         let expbuff = Buffer::<Ulong4>::builder().queue(pq.queue().clone())
             .flags(MemFlags::new().read_write()).len(n)
             .build()?;
-        let resbuff = Buffer::<G1ProjectiveStruct>::builder().queue(pq.queue().clone())
+        let g1resbuff = Buffer::<G1ProjectiveStruct>::builder().queue(pq.queue().clone())
+            .flags(MemFlags::new().read_write()).len(n)
+            .build()?;
+        let g2basebuff = Buffer::<G2AffineStruct>::builder().queue(pq.queue().clone())
+            .flags(MemFlags::new().read_write()).len(n)
+            .build()?;
+        let g2resbuff = Buffer::<G2ProjectiveStruct>::builder().queue(pq.queue().clone())
             .flags(MemFlags::new().read_write()).len(n)
             .build()?;
         let dmbuff = Buffer::<Uchar>::builder().queue(pq.queue().clone())
             .flags(MemFlags::new().read_write()).len(n)
             .build()?;
-        Ok(MultiexpKernel {proque: pq, g1_base_buffer: g1basebuff, g1_result_buffer: resbuff,
+        Ok(MultiexpKernel {proque: pq,
+            g1_base_buffer: g1basebuff, g1_result_buffer: g1resbuff,
+            g2_base_buffer: g2basebuff, g2_result_buffer: g2resbuff,
             exp_buffer: expbuff, dm_buffer: dmbuff})
     }
 
@@ -83,7 +105,29 @@ impl MultiexpKernel {
             let mut tres = unsafe { std::mem::transmute::<&mut [<G as CurveAffine>::Projective], &mut [G1ProjectiveStruct]>(&mut res) };
             self.g1_result_buffer.read(tres).enq()?;
             return Ok((res[0]))
-        } else {
+        } else if sz == 200 {
+            let bases = unsafe { std::mem::transmute::<Arc<Vec<G>>,Arc<Vec<G2Affine>>>(bases) }.to_vec();
+            let tbases = unsafe { std::mem::transmute::<&[G2Affine], &[G2AffineStruct]>(&bases[..]) };
+            self.g2_base_buffer.write(tbases).enq()?;
+            self.exp_buffer.write(texps).enq()?;
+            self.dm_buffer.write(tdm).enq()?;
+            let kernel = self.proque.kernel_builder("G2_batched_multiexp")
+                .global_work_size([1])
+                .arg(&self.g2_base_buffer)
+                .arg(&self.g2_result_buffer)
+                .arg(&self.exp_buffer)
+                .arg(&self.dm_buffer)
+                .arg(skip as u32)
+                .arg(tbases.len() as u32)
+                .arg(texps.len() as u32)
+                .build()?;
+            unsafe { kernel.enq()?; }
+            let mut res = [<G as CurveAffine>::Projective::zero()];
+            let mut tres = unsafe { std::mem::transmute::<&mut [<G as CurveAffine>::Projective], &mut [G2ProjectiveStruct]>(&mut res) };
+            self.g2_result_buffer.read(tres).enq()?;
+            return Ok((res[0]))
+        }
+        else {
             Err(GPUError {msg: "Only Bls12-381 G1 is supported!".to_string()} )
         }
     }
