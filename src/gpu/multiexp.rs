aa27f2a3336b33f4ac085e71cbc94f3286cb470c
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-10-22 15:21:17 +0330

    back to 15mm chunk size

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -20,7 +20,7 @@ use crate::multiexp::{multiexp as cpu_multiexp, FullDensity};
 const NUM_GROUPS : usize = 334; // Partition the bases into `NUM_GROUPS` groups
 const WINDOW_SIZE : usize = 10; // Exponents are 255bit long, divide exponents into `WINDOW_SIZE` bit windows
 const NUM_WINDOWS : usize = 26; // Then we will have Ceil(256/`WINDOW_SIZE`) windows per exponent
-const CHUNK_SIZE : usize = 25_000_000; // Maximum number of base elements we can pass to a GPU
+const CHUNK_SIZE : usize = 15_000_000; // Maximum number of base elements we can pass to a GPU
 const SPEEDUP : f64 = 3.1f64; // Speedup of a single GPU compared to CPU
 // So each group will have `NUM_WINDOWS` threads and as there are `NUM_GROUPS` groups, there will
 // be `NUM_GROUPS` * `NUM_WINDOWS` threads in total.
