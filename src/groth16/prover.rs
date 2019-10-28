1d7e8a658cf91bd4dcc85ce263edfd93228eded1
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-10-29 02:29:31 +0330

    remove zero-knowledge part of the prover

--- src/groth16/prover.rs
+++ src/groth16/prover.rs
@@ -296,24 +296,6 @@ where
     let b_aux_density = Arc::new(prover.b_aux_density);
     let b_aux_density_total = b_aux_density.get_total_density();
 
-    let (b_g1_inputs_source, b_g1_aux_source) =
-        params.get_b_g1(b_input_density_total, b_aux_density_total)?;
-
-    let b_g1_inputs = multiexp(
-        &worker,
-        b_g1_inputs_source,
-        b_input_density.clone(),
-        input_assignment.clone(),
-        &mut multiexp_kern
-    );
-    let b_g1_aux = multiexp(
-        &worker,
-        b_g1_aux_source,
-        b_aux_density.clone(),
-        aux_assignment.clone(),
-        &mut multiexp_kern
-    );
-
     let (b_g2_inputs_source, b_g2_aux_source) =
         params.get_b_g2(b_input_density_total, b_aux_density_total)?;
 
@@ -332,33 +314,17 @@ where
         return Err(SynthesisError::UnexpectedIdentity);
     }
 
-    let mut g_a = vk.delta_g1.mul(r);
-    g_a.add_assign_mixed(&vk.alpha_g1);
-    let mut g_b = vk.delta_g2.mul(s);
-    g_b.add_assign_mixed(&vk.beta_g2);
-    let mut g_c;
-    {
-        let mut rs = r;
-        rs.mul_assign(&s);
-
-        g_c = vk.delta_g1.mul(rs);
-        g_c.add_assign(&vk.alpha_g1.mul(s));
-        g_c.add_assign(&vk.beta_g1.mul(r));
-    }
+    let mut g_a = vk.alpha_g1.into_projective();
+    let mut g_b = vk.beta_g2.into_projective();
+    let mut g_c = E::G1::zero();
     let mut a_answer = a_inputs.wait()?;
     a_answer.add_assign(&a_aux.wait()?);
     g_a.add_assign(&a_answer);
-    a_answer.mul_assign(s);
-    g_c.add_assign(&a_answer);
 
-    let mut b1_answer = b_g1_inputs.wait()?;
-    b1_answer.add_assign(&b_g1_aux.wait()?);
     let mut b2_answer = b_g2_inputs.wait()?;
     b2_answer.add_assign(&b_g2_aux.wait()?);
 
     g_b.add_assign(&b2_answer);
-    b1_answer.mul_assign(r);
-    g_c.add_assign(&b1_answer);
     g_c.add_assign(&h.wait()?);
     g_c.add_assign(&l.wait()?);
 
